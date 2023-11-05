db0a1a5590c7be740dfed0989d59236528cb94a1
Trixi Jansuy <7642477+TJansuy@users.noreply.github.com>
2023-11-04 22:20:07 -0700

    Ensuring div is being created, and adding a filter to prevent empty entries from being rendered

--- home.tsx
+++ home.tsx
@@ -1,10 +1,14 @@
 const createElements = (links: string[][]) => {
     const root = document.getElementById("LinkContainer")
     links.map((val) => {
-        let element = document.createElement("a");
-        element.appendChild(document.createTextNode(val[0]));
-        element.setAttribute("href", val[1]);
-        root?.appendChild(document.createElement("div").appendChild(element));
+        if (val[0] && val[1]) {
+            let element = document.createElement("a");
+            element.appendChild(document.createTextNode(val[0]));
+            element.setAttribute("href", val[1]);
+            let container = document.createElement("div");
+            container.appendChild(element);
+            root?.appendChild(container);
+        }
     })
 };
 
