7bdb401fd11439d991606e41aa7e66000ac2f949
Trixi <7642477+TJansuy@users.noreply.github.com>
2024-01-08 07:13:21 +0000

    Adding QR code creation logic

--- home.tsx
+++ home.tsx
@@ -1,13 +1,51 @@
-const createElements = (links: string[][]) => {
-    const root = document.getElementById("LinkContainer")
-    links.map((val) => {
-        if (val[0] && val[1]) {
-            let element = document.createElement("a");
-            element.appendChild(document.createTextNode(val[0]));
-            element.setAttribute("href", val[1]);
+import { BrowserQRCodeSvgWriter } from '@zxing/browser'
 
-            let container = document.createElement("div");
+type qrType = {
+    width: number,
+    height: number
+}
+
+type qrLink = {text: string, link: string, qr?: qrType};
+
+const QRType1: qrType = {
+    width: 256,
+    height: 256
+}
+
+const parseQrLink = (value: qrLink): HTMLElement => {
+    // Construct the clickable Link element
+    let element: HTMLElement = document.createElement("a");
+    element.appendChild(document.createTextNode(value.text));
+    element.setAttribute("href", value.link);
+    
+    // If there are QR Options, attempt to create a QR Code.
+    if (value?.qr) {
+        try {
+            // Attempt to generator the QR Code using ZXing/browser
+            const qr = new BrowserQRCodeSvgWriter().write(value.link, value.qr.width, value.qr.height)
+            
+            // Store it in an element above the link
+            const container = document.createElement('div');
+            container.setAttribute('class', 'QR-Code');
+            container.appendChild(qr);
             container.appendChild(element);
+
+            element = container;
+        } catch (ignored) {
+            // Oh well
+            console.log('Unable to create QR Code!: ' + ignored)
+        }
+    }
+
+    return element;
+}
+
+const createElements = (links: qrLink[]): void => {
+    const root = document.getElementById("LinkContainer")
+    links.map((val: qrLink) => {
+        if (val.text && val.link) {
+            const container = document.createElement("div");
+            container.appendChild(parseQrLink(val));
             container.setAttribute("class", "Link")
             root?.appendChild(container);
         }
@@ -15,8 +53,15 @@ const createElements = (links: string[][]) => {
 };
 
 const links = [
-    ["My github", "https://github.com/TJansuy"],
-    ["My LinkedIn", "https://www.linkedin.com/in/trixi-jansuy/"],
-    ["", ""],
+    {
+        text: "My github", 
+        link: "https://github.com/TJansuy",
+    },
+    {
+        text: "My LinkedIn", 
+        link: "https://www.linkedin.com/in/trixi-jansuy/",
+        QRType1
+    },
 ];
+
 createElements(links)
