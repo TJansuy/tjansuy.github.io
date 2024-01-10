1ed2d0b2bf40a690db51a257172780f4b99a9532
Trixi Jansuy <7642477+TJansuy@users.noreply.github.com>
2024-01-09 22:31:45 -0800

    Adjusting approach from Client Side rendering to Server Side

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 tsc
+node home.js
--- home.tsx
+++ home.tsx
@@ -1,70 +1,50 @@
-import { BrowserQRCodeSvgWriter } from '@zxing/browser'
+import QRCode from 'qrcode';
+import * as fs from 'fs';
 
-type qrType = {
-    width: number,
-    height: number
-}
+const Links = [
+    {content: 'Trixi Jansuy', style: 'Name'},
+    {content: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/trixi-jansuy/',
+    qr: {height: 256, width: 256},
+    style: 'linkedin'},
+    {content: 'Github', 
+    href: 'https://github.com/TJansuy',
+    style: 'github'},
+];
 
-type qrLink = {text: string, link: string, qr?: qrType};
+type LinksType = {content: string, href?: string, qr?:{height: number, width: number}, style?: string};
 
-const QRType1: qrType = {
-    width: 256,
-    height: 256
-}
+const createElements = async (links: LinksType[]) => {
+    let toReturn: string = "";
 
-const parseQrLink = (value: qrLink): HTMLElement => {
-    // Construct the clickable Link element
-    let element: HTMLElement = document.createElement('a');
-    element.appendChild(document.createTextNode(value.text));
-    element.setAttribute('href', value.link);
-    element.setAttribute('id', '')
-    
-    // If there are QR Options, attempt to create a QR Code.
-    if (value?.qr) {
-        try {
-            // Attempt to generator the QR Code using ZXing/browser
-            const qr = new BrowserQRCodeSvgWriter().write(value.link, value.qr.width, value.qr.height)
-            
-            // Store it in an element above the link
-            const container = document.createElement('div');
-            container.setAttribute('class', 'QR-Code');
-            container.appendChild(qr);
-            container.appendChild(element);
-
-            element = container;
-        } catch (ignored) {
-            // Oh well
-            console.log('Unable to create QR Code!: ' + ignored)
+    for (const index in links) {
+        const e = links[index];
+        if(e.content && e.href) {
+            if (e.qr && e.qr.height && e.qr.width) {
+                const qr = await QRCode.toString(e.href, {type: "svg", height: e.qr.height, width: e.qr.width, errorCorrectionLevel: "H"} as QRCode.QRCodeToStringOptions);
+                toReturn += `<div class="QR">${qr}</div>`;
+            }
+            toReturn += `<a class="Link${e.style ? ' ' + e.style : ''}" href="${e.href}"><div>${e.content}</div></a>`;
+        } else if (e.content && e.style){
+            toReturn += `<div class="${e.style}">${e.content}</div>`
         }
     }
 
-    return element;
-}
-
-const createElements = (links: qrLink[]): void => {
-    const root = document.getElementById('LinkContainer')
-    links.map((val: qrLink) => {
-        if (val.text && val.link) {
-            const container = document.createElement('div');
-            container.appendChild(parseQrLink(val));
-            container.setAttribute('class', 'Link')
-            root?.appendChild(container);
-        }
-    })
+    return toReturn;
 };
 
-const links = [
-    {
-        text: 'My LinkedIn', 
-        link: 'https://www.linkedin.com/in/trixi-jansuy/',
-        qr: QRType1
-    },
-    {
-        text: 'My github', 
-        link: 'https://github.com/TJansuy',
-    },
-];
-
-document.addEventListener('DOMContentLoaded', () => {
-    createElements(links);
-})
+createElements(Links).then((links) => {
+    fs.writeFile('./index.html', 
+    `<!DOCTYPE html>
+    <html>
+        <head>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/brands.min.css"/>
+            <link rel="stylesheet" href="index.css">
+        </head>
+        <body>
+            <div id="LinkContainer">
+                ${links}
+            </div>
+        </body>
+    </html>`, () => {});
+});
